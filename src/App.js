2bf323778ac768f36b26ccc4e7147d4af675f3f8
mrinex <mrinex@mail.ru>
2020-07-28 01:23:15 +0300

    feat: add style

--- src/App.js
+++ src/App.js
@@ -2,30 +2,14 @@ import React from 'react';
 import {
   BrowserRouter, Switch, Route, Redirect
 } from 'react-router-dom';
-import TypeButton from './components/typebutton.header';
-
-const typeBirds = [
-  ['Разминка', '/warm-up'],
-  ['Воробьиные', '/passerines'],
-  ['Лесные птицы', '/forest_birds'],
-  ['Певчие птицы', '/song_birds'],
-  ['Хищные птицы', '/predator_birds'],
-  ['Морские птицы', '/sea_birds']
-];
+import Header from './header/header';
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <div>Songbird</div>
-        <div>Score: <span>0</span></div>
-        {typeBirds.map((birds, index) => {
-          const [type, link] = birds;
-          return <TypeButton title={type} link={link}/>
-        })}
-      </header>
+      <Header />
       <Switch>
-        <div className="App">
+        <div className="app">
           разминка информация
         </div>
       </Switch>
---
+++ src/header/header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import TypeButton from './typebutton.header';
+import './header.scss';
+
+const typeBirds = [
+  ['Разминка', '/warm-up'],
+  ['Воробьиные', '/passerines'],
+  ['Лесные птицы', '/forest_birds'],
+  ['Певчие птицы', '/song_birds'],
+  ['Хищные птицы', '/predator_birds'],
+  ['Морские птицы', '/sea_birds']
+];
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header_score">
+        <div>Songbird</div>
+        <div>Score: <span>0</span></div>
+      </div>
+      <div className="header_type-birds">
+        {typeBirds.map((birds, index) => {
+          const [type, link] = birds;
+          return <TypeButton title={type} link={link} key={`${index.toString()}header`}/>
+        })}
+      </div>
+    </header>
+  )
+}
+
+export default Header;
