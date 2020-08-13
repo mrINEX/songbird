918516429180ad52e162a2ff0461a9f2f781d549
mrinex <mrinex@mail.ru>
2020-08-13 20:31:33 +0300

    docs: homepage

---
+++ docs/precache-manifest.8b20809d1dc33e0116b6c79e784db28a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00cbfbebad8004a8efc01d6903becf9",
+    "url": "/songbird/index.html"
+  },
+  {
+    "revision": "a06124dfebc0074bab3b",
+    "url": "/songbird/static/css/main.b66f361e.chunk.css"
+  },
+  {
+    "revision": "5d1675175544d3f2f5f2",
+    "url": "/songbird/static/js/2.b88be8a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/songbird/static/js/2.b88be8a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a06124dfebc0074bab3b",
+    "url": "/songbird/static/js/main.6c9c6f3d.chunk.js"
+  },
+  {
+    "revision": "41fc37381d3a97757444",
+    "url": "/songbird/static/js/runtime-main.710c5ffc.js"
+  },
+  {
+    "revision": "566392102338d430515329e400457025",
+    "url": "/songbird/static/media/bird-harrier.56639210.jpg"
+  },
+  {
+    "revision": "dce55dcdbaf556d77afca3af7d624a6c",
+    "url": "/songbird/static/media/bird-pelican.dce55dcd.jpg"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "/songbird/static/media/birdnone.06a46938.jpg"
+  },
+  {
+    "revision": "8e3d6124f258a1690567f02ade38cd2d",
+    "url": "/songbird/static/media/correct.8e3d6124.mp3"
+  },
+  {
+    "revision": "32fc22b2ceef21006a7f31e18e44f1f3",
+    "url": "/songbird/static/media/error.32fc22b2.mp3"
+  }
+]);
